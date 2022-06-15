30066f780599e64898cdeafb6a3d2b520542edcc
Irene Valle <irenevalleguillen@gmail.com>
2022-06-15 16:44:12 +0200

    drop down menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,65 +1,106 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 
 function Navbar(props) {
+  let [open, setOpen] = useState(false);
+
+  const handleBurger = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+
   return (
-    <div className='relative'>
-    <nav className="sticky flex items-center justify-between py-3 bg-[#361201] text-gray-200 shadow-md shadow-[#361201a2] navbar navbar-expand-lg navbar-light">
-      <ul className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-        <li className="collapse navbar-collapse flex-grow items-center">
-          <NavLink className="text-xl text-white pr-2 font-semibold" to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className="collapse navbar-collapse flex-grow items-center">
-          <NavLink className="text-xl text-white pr-2 font-semibold" to="/offers">
-            Offers
-          </NavLink>
-        </li>
-        <li className="collapse navbar-collapse flex-grow items-center">
+    <div className="sticky top-0">
+      <nav className="flex items-center justify-around py-3 bg-[#361201] text-gray-200 shadow-md shadow-[#361201a2] navbar navbar-expand-lg navbar-light">
+        <NavLink className="text-xl text-white pr-2 font-semibold" to="/">
+          Hour Hood
+        </NavLink>
+
+        <div>
           <NavLink
             className="text-xl text-white pr-2 font-semibold"
-            to="/profile"
+            to="/offers"
           >
-            Profile
+            Offers
           </NavLink>
-        </li>
-        <li className="collapse navbar-collapse flex-grow items-center">
           <NavLink
             className="text-xl text-white pr-2 font-semibold"
             to="/requests"
           >
             Requests
           </NavLink>
-        </li>
-        <li className="collapse navbar-collapse flex-grow items-center">
           <NavLink
             className="text-xl text-white pr-2 font-semibold"
             to="/bookings"
           >
             Bookings
           </NavLink>
-        </li>
-        <li
-          className="text-xl text-white pr-2 font-semibold"
-          onClick={(e) => props.switchUserCb(null)}
-        >
-          Anonymous User | {' '}
-        </li>
-        <li
-          className="text-xl text-white pr-2 font-semibold"
-          onClick={(e) => props.switchUserCb(1)}
-        >
-          Default User1 | {' '}
-        </li>
-        <li
-          className="text-xl text-white pr-2 font-semibold"
-          onClick={(e) => props.switchUserCb(2)}
-        >
-          Default User2
-        </li>
-      </ul>
-    </nav>
+        </div>
+
+        <div className="flex">
+          <NavLink
+            className="text-xl text-white pr-2 font-semibold"
+            to="/profile"
+          >
+            Profile
+          </NavLink>
+        </div>
+
+        <div className="flex flex-col">
+          <div>
+            <div
+              onClick={handleBurger}
+              className=" items-center justify-center cursor-pointer h-8"
+            >
+              <h3
+                className={` text-center px-6 text-xl font-semibold pt-[10px] transform duration-200 ease-in-out -translate-y-2 ${open}`}
+              >
+                Users
+              </h3>
+              {/* <div
+                className={`w-10 h-1 bg-white rounded-md shadow-md transform duration-200 ease-in-out -translate-y-2 ${
+                  open && "rotate-45 translate-y-full"
+                }`}
+              ></div>
+              <div
+                className={`w-10 h-1 bg-white rounded-md shadow-md transform duration-75 ease-in-out ${
+                  open && "-translate-x-14 opacity-0 shadow-none"
+                }`}
+              ></div>
+              <div
+                className={`w-10 h-1 bg-white rounded-md shadow-md transform duration-200 ease-in-out translate-y-2 ${
+                  open && "-rotate-45 -translate-y-full"
+                }`}
+              ></div> */}
+            </div>
+            <div className="relative">
+              {open && (
+                <div className="flex items-center justify-center absolute flex-col">
+                  <button
+                    className="text-white bg-[#361201dd] hover:bg-[#361201] hover:text-[#FF9940] px-10 py-4 font-semibold"
+                    onClick={(e) => props.switchUserCb(null)}
+                  >
+                    Anonymous{" "}
+                  </button>
+                  <button
+                    className=" text-white bg-[#361201dd] px-16 py-4 hover:bg-[#361201] hover:text-[#FF9940] font-medium"
+                    onClick={(e) => props.switchUserCb(1)}
+                  >
+                    User1{" "}
+                  </button>
+                  <button
+                    className="text-white bg-[#361201dd] px-[63px] py-4 rounded-b-lg hover:text-[#FF9940] hover:bg-[#361201] font-medium"
+                    onClick={(e) => props.switchUserCb(2)}
+                  >
+                   User2
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
