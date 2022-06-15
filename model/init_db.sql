c8c8aef99c867b16c36c70a5cb7c5d9b08bb6837
Lucie-Baborova <lucie.babor@gmail.com>
2022-06-15 10:03:57 +0200

    in init-db users, service_post, and servie_categories are working

--- model/init_db.sql
+++ model/init_db.sql
@@ -20,24 +20,29 @@ CREATE TABLE users (
 
 INSERT INTO users (first_name, last_name, street, house_number, city_code, city_name, country, email, user_description, photo) VALUES ('John', 'Smith', 'Carrer de Grassot', '101-A', 08025, 'Barcelona', 'Spain','johnsmith14062022@gmail.com', "Hi , I love pop-corn and see films with my family on Fridays", ""), ('Karen', 'Magnamara', 'Carrer de Grassot', '85-B', 08025, 'Barcelona', 'Spain','karenmagnamara14062022@gmail.com', "Hi , I'm Karen I would like to be able to have more time to spend with my child.", "");
 
+CREATE TABLE service_categories (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    category_title VARCHAR(100), 
+    photo VARCHAR(100)
+); 
+
+INSERT INTO service_categories (category_title, photo) 
+VALUES ('Food',''), ('Home Services & Repairs',''), ('Health & Wellness', ''), ('Hobbies', ''), ('Transport', ''), ('Education', ''), ('IT', ''), ('Children & Pets', ''), ('Bureaucracy', ''), ('Others', ''); 
+
 CREATE TABLE service_post (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     service_title VARCHAR(100),
     service_description TEXT(255),
     capacity INTEGER NOT NULL,
-    category_id INT NOT NULL,
-    provider_id INT,
-    FOREIGN KEY (provider_id) REFERENCES users (id) ON DELETE SET NULL 
+    fk_category_id INT,
+    fk_provider_id INT,
+    FOREIGN KEY (fk_provider_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (fk_category_id) REFERENCES service_categories (id) ON DELETE SET NULL
 );
 
-INSERT INTO service_post (service_title, service_description, capacity, category_id, provider_id) 
+INSERT INTO service_post (service_title, service_description, capacity, fk_category_id, fk_provider_id) 
 VALUES ('Babysitting', 'I am open to spend time this week to take care of your children if you need it.', 2, 8, 1); 
 
 
-CREATE TABLE service_categories (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    category_title VARCHAR(100), 
-    photo VARCHAR(100),
-    FOREIGN KEY (id) REFERENCES service_post (category_id) ON DELETE SET NULL
-); 
+
 
