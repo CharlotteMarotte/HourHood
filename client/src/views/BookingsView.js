d590a0f52151f928384ea43c9bf7a81039e7ebfe
Charlotte Bilsing <Charlotte.Bilsing@posteo.de>
2022-06-16 15:36:05 +0200

    Bookings/Requests cards

--- client/src/views/BookingsView.js
+++ client/src/views/BookingsView.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
+import SelectedOfferCard from '../components/SelectedOfferCard';
 
 export default function BookingsView() {
   return (
-    <div>BookingsView</div>
-    // https://codepen.io/egoistdeveloper/pen/xxYrmgd potential code
-  )
+    <SelectedOfferCard/>
+  );
 }
--- client/src/views/ProfileView.js
+++ client/src/views/ProfileView.js
@@ -22,70 +22,79 @@ export default function ProfileView() {
   return (
     // Code thanks to https://codepen.io/tariq01/pen/jOyLrRJ
     <>
-    {user ? (
-    <div className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover">     
-      <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-24 lg:my-0">
-        <div className="w-full lg:w-2/5">
-          <img
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F96%2F24%2Fa8%2F9624a8213b44121b85d6139e3dad1827.png&f=1&nofb=1"
-            className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
-          />
-        </div>
-        <div
-          id="profile"
-          className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none bg-white opacity-75 mx-6 lg:mx-0"
-        >
-          <div className="p-4 md:p-12 text-center lg:text-left">
-            <h1 className="text-3xl font-bold pt-8 lg:pt-0">My Profile</h1>
-            <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-amber-500 opacity-25"></div>
+      {user ? (
+        <div className="font-sans antialiased leading-normal tracking-wider text-gray-900 bg-cover">
+          <div className="flex flex-wrap items-center h-auto max-w-4xl mx-auto my-24 lg:h-screen lg:my-0">
+            <div className="w-full lg:w-2/5">
+              <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F96%2F24%2Fa8%2F9624a8213b44121b85d6139e3dad1827.png&f=1&nofb=1"
+                className="hidden rounded-none shadow-lg lg:rounded-lg lg:block"
+              />
+            </div>
+            <div
+              id="profile"
+              className="w-full mx-6 bg-white rounded-lg shadow-lg opacity-75 lg:w-3/5 lg:rounded-r-lg lg:rounded-l-none lg:mx-0 "
+            >
+              <div className="p-4 text-center md:p-12 lg:text-left">
+                <h1 className="pt-8 text-3xl font-bold lg:pt-0">My Profile</h1>
+                <div className="w-4/5 pt-3 mx-auto border-b-2 opacity-25 lg:mx-0 border-amber-500"></div>
 
-            <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-              About Me{' '}
-            </p>
-            <p className="text-sm">
-              I moved to Gracia in September with my partner.
-            </p>
+                <p className="flex items-center justify-center pt-4 text-base font-bold lg:justify-start">
+                  About Me{' '}
+                </p>
+                <p className="text-sm">
+                  I moved to Gracia in September with my partner.
+                </p>
 
-            <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-              My Hobbies{' '}
-            </p>
-            <p className="text-sm">Crocheting, gardening, ceramics</p>
+                <p className="flex items-center justify-center pt-4 text-base font-bold lg:justify-start">
+                  My Hobbies{' '}
+                </p>
+                <p className="text-sm">Crocheting, gardening, ceramics</p>
 
-            <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-              My Superpower is...{' '}
-            </p>
-            <p className="text-sm">Planning suprise parties</p>
+                <p className="flex items-center justify-center pt-4 text-base font-bold lg:justify-start">
+                  My Superpower is...{' '}
+                </p>
+                <p className="text-sm">Planning suprise parties</p>
 
-            <div className="pt-12 pb-8">
-              <Link to="edit" className="bg-amber-700 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded-full">
-                Edit Profile{' '}
-              </Link>
+                <div className="pt-12 pb-8">
+                  <Link
+                    to="edit"
+                    className="px-4 py-2 font-bold text-white rounded-full bg-amber-700 hover:bg-amber-900"
+                  >
+                    Edit Profile{' '}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <h1 className="pt-8 text-3xl font-bold lg:pt-0">
+              My current offerings
+            </h1>
+            <div className="w-4/5 py-3 mx-auto border-b-2 opacity-25 lg:mx-0 border-amber-500"></div>
+            <Link
+              to={'/service-post'}
+              className="block w-1/4 p-4 mx-auto mt-5 overflow-hidden text-white border-2 rounded-lg bg-lime-600 h-1/6 border-lime-800"
+            >
+              Add a new offer +
+            </Link>
+            <div className="flex flex-wrap px-5 py-24 mx-auto -m-4 space-x-6 md:block lg:flex">
+              {offers
+                .filter((e) => e.providerId === user.id)
+                .map((o) => (
+                  <OfferCard key={o.id} offer={o} view={'profile'} />
+                ))}
             </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <h1 className="text-3xl font-bold pt-8 lg:pt-0">
-          My current offerings
-        </h1>
-        <div className="mx-auto lg:mx-0 w-4/5 py-3 border-b-2 border-amber-500 opacity-25"></div>
+      ) : (
         <Link
-          to={'/service-post'}
-          className="block bg-lime-600 mt-5 p-4 w-1/4 h-1/6 mx-auto border-2 border-lime-800 rounded-lg overflow-hidden text-white"
+          className="p-4 my-1 text-2xl font-medium rounded-lg title-font bg-amber-200 text-amber-900"
+          to="/offers"
         >
-          Add a new offer +
+          No User logged in - back to offers
         </Link>
-        <div className="px-5 py-24 mx-auto md:block lg:flex space-x-6 flex flex-wrap -m-4">
-          {offers
-            .filter((e) => e.providerId === user.id)
-            .map((o) => (
-              <OfferCard key={o.id} offer={o} view={'profile'} />
-            ))}
-          
-        </div>
-      </div>
-    </div>
-) : <Link className="title-font text-2xl font-medium bg-amber-200 p-4 rounded-lg text-amber-900 my-1" to="/offers">No User logged in - back to offers</Link>}
-</>
+      )}
+    </>
   );
 }
