3472a725631ff4e26814fda9a16f7a6311509e0b
Irene Valle <irenevalleguillen@gmail.com>
2022-06-27 11:12:06 +0200

    mergin

--- client/src/views/ProfileView.js
+++ client/src/views/ProfileView.js
@@ -108,9 +108,9 @@ export default function ProfileView(props) {
   return (
     // Code thanks to https://codepen.io/tariq01/pen/jOyLrRJ
     <>
-    {/* only show Profile page if user is logged in */}
-      {user ? (
-
+     {/* only show Profile page if user is logged in */}
+       {user ? (
+/*
         <div className="font-sans antialiased leading-normal tracking-wider bg-cover">
           {user.id !== myData.id && <GoToOfferButton />}
           <h3 className="md:text-5xl text-2xl text-[#361201] font-bold mt-6">
@@ -172,8 +172,8 @@ export default function ProfileView(props) {
                     {myData.superpower ? myData.superpower : "No superpower"}
                   </p>
                 </div>
-                {/* only show when profile of user who is currently logged in is shown */}
-                {user.id === myData.id && (
+                {/* only show when profile of user who is currently logged in is shown */
+                /* {user.id === myData.id && (
                   <div className="pt-12 pb-8">
                     <Link
                       to="/profile/edit"
@@ -182,9 +182,9 @@ export default function ProfileView(props) {
                       Edit Profile{" "}
                     </Link>
                   </div>
-                )}
-/*
-        <div className="font-sans antialiased leading-normal tracking-wider text-gray-900 bg-cover">
+                )} */ 
+
+        <div className="font-sans antialiased leading-normal text-[#361201] tracking-wide bg-cover">
           <div className="flex flex-wrap items-center h-auto max-w-4xl mx-auto my-24 lg:h-screen lg:my-0">
             <div className="w-full lg:w-2/5">
               {Number(id) === user.id && 
@@ -205,11 +205,18 @@ export default function ProfileView(props) {
               className="w-full mx-6 bg-white rounded-lg shadow-lg opacity-75 lg:w-3/5 lg:rounded-r-lg lg:rounded-l-none lg:mx-0 "
             >
               <div className="p-4 text-center md:p-12 lg:text-left">    
-                <h1 className="pt-8 text-3xl font-bold lg:pt-0">
-                  {Number(id) === user.id ? `${user.first_name}'s Profile`
-                    : `${myData.first_name}'s Profile`
+                <h1 className="pt-8 text-3xl font-bold text-[#fe8923] lg:pt-0">
+                  {Number(id) === user.id ? <h1 className="pt-8 text-3xl font-bold text-[#fe8923] lg:pt-0">
+                    {myData.first_name}'s{" "}
+                    <span className="text-[#361201]">Profile</span>
+                  </h1>
+                    : <h1 className="pt-8 text-3xl font-bold text-[#fe8923] lg:pt-0">
+                    {myData.first_name}'s{" "}
+                    <span className="text-[#361201]">Profile</span>
+                  </h1>
                   }
                 </h1>
+                
                 <div className="w-4/5 pt-3 mx-auto border-b-2 opacity-25 lg:mx-0 border-amber-500"></div>
 
                 <p className="flex items-center justify-center pt-4 text-base font-bold lg:justify-start">
@@ -253,48 +260,21 @@ export default function ProfileView(props) {
                    </p>}
                 
               </div>
-              {/* only show when profile of user who is currently logged in is shown */}
+             
               {user.id === Number(id) && (
                 <div className="pt-12 pb-8">
-                  <Link
-                    to="/profile/edit"
-                    className="px-4 py-2 font-bold text-white rounded-full bg-amber-700 hover:bg-amber-900"
-                  >
-                    Edit Profile{' '}
-                  </Link>
-                </div>
-              )}
-            {user.id === Number(id) && (
-              <div className="pt-6 pb-12">
-                <button
-                  title="With a token you can invite a friend to become part of the community!"
-                  className="px-4 py-2 font-bold text-white rounded-full bg-amber-700 hover:bg-amber-900"
-                  onClick={(e) => getMyToken()}
+                <Link
+                  to="/profile/edit"
+                  className="px-10 py-3 text-lg font-bold uppercase text-white hover:shadow-lg hover:shadow-[#ff994091] rounded-xl bg-[#ff9940e3] hover:bg-[#fe8923]"
                 >
-                  Get a token{' '}
-
-                </button>
-                <p className={`w-1/4 p-3 mx-auto mt-3 font-mono ${myToken.length>0 && "bg-gray-200 hover:bg-gray-100"}`}>{myToken}</p>
- */
+                  Edit Profile{" "}
+                </Link>
               </div>
               )}
             </div>
           </div>
-          <div className="pt-12 pb-8 lg:text-right -mt-28 md:-mt-24 lg:mr-64 mb-2 lg:-mt-40">
-            <p className="text-[#361201] text-lg font-semibold">
-              Do you want to invite a friend? <br />
-              <span className="text-[#fe8923]">Send him/her a token</span>
-            </p>
+          
 
-            <button
-              className="px-6 py-2 mt-4 text-lg font-bold uppercase text-[#361201] rounded-xl hover:shadow-lg hover:shadow-[#ff994091] border-2 border-[#FFE500] bg-[#FFF7A3] hover:bg-[#FFE500]"
-              onClick={(e) => getMyToken()}
-            >
-              Get token{" "}
-            </button>
-            <p className="font-semibold lg:mr-5 mt-2">{myToken}</p>
-          </div>
-          {/* only show when profile of user who is currently logged in is shown */}
           {user.id === Number(id) && (
             <div>
               <div className="container mx-auto">
@@ -325,6 +305,7 @@ export default function ProfileView(props) {
           </Link>
         </div>
       )}
+
     </>
   );
 }
